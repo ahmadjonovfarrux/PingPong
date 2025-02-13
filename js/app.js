cd79d512e1302b66e3503da7255d6d0826b7519a
Farrux <Farrux@gmail.com>
2025-02-13 22:35:09 +0500

    refactor CSS and HTML structure for improved organization and clarity

--- js/app.js
+++ js/app.js
@@ -67,4 +67,13 @@
 //   );
 // });
 
-// 3-mashq
+// // 3-mashq
+// const button = document.querySelector("#addElementBtn");
+// const ul = document.querySelector("ul");
+// button.addEventListener("click", () => {
+//   const div = document.createElement("div");
+//   div.textContent =
+//     "Our group is trying to be one of the most successesfull group in this academy";
+//   ul.appendChild(div);
+// });
+
