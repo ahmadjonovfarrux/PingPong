a054169672d194acce6796978497960416984370
Farrux <Farrux@gmail.com>
2025-02-25 22:01:09 +0500

    refactor styles and HTML structure for improved layout and interactivity; added winner score selection and reset functionality

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ const player2 = document.querySelector("#player2");
 const player1Btn = document.querySelector("#player1Btn");
 const player2Btn = document.querySelector("#player2Btn");
 const winnerScore = document.querySelector("#winnerScore");
-// const resetKey = prompt("Enter the limit of the time");
+const reset = document.querySelector("#reset");
 
 let player1Counter = 0;
 let player2Counter = 0;
+let scoreLimit = +winnerScore.value;
 let game = true;
 
 player1Btn.addEventListener("click", () => {
   if (game) {
-    if (winnerScore.textContent > player1Counter) {
-      player1Counter++;
-      player1.textContent = player1Counter;
-    } else {
+    player1Counter++;
+    player1.textContent = player1Counter;
+    if (scoreLimit == player1Counter) {
       game = false;
       player1.style.color = "green";
       player2.style.color = "red";
@@ -24,15 +24,32 @@ player1Btn.addEventListener("click", () => {
 
 player2Btn.addEventListener("click", () => {
   if (game) {
-    if (winnerScore.textContent > player2Counter) {
-      player2Counter++;
-      player2.textContent = player2Counter;
-    } else {
+    player2Counter++;
+    player2.textContent = player2Counter;
+    if (scoreLimit == player2Counter) {
       game = false;
       player2.style.color = "green";
       player1.style.color = "red";
     }
   }
+});
+function resetFunction() {
+  player1.textContent = 0;
+  player1Counter = 0;
+  player2.textContent = 0;
+  player2Counter = 0;
+  game = true;
+  player2.style.color = "white";
+  player1.style.color = "white";
+}
+
+winnerScore.addEventListener("change", () => {
+  scoreLimit = +winnerScore.value;
+  resetFunction();
+});
+
+reset.addEventListener("click", () => {
+  resetFunction();
 });
 
 // Event loop
